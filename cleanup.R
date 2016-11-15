edd69147ce8b4d034d9fb15606375c9a345e5f38
jennybc <jenny@stat.ubc.ca>
2016-11-15 13:01:53 -0800

    another wave of photos

--- cleanup.R
+++ cleanup.R
@@ -3,16 +3,20 @@ library(stringr)
 library(tidyverse)
 
 ## rename first wave to have 3 digit number vs 2
-fig_files <- list.files(pattern = "lego-rstats.*\\.jpg$")
-file.rename(fig_files, str_replace(fig_files, "(\\d{2})", "0\\1"))
+# fig_files <- list.files(pattern = "lego-rstats.*\\.jpg$")
+# file.rename(fig_files, str_replace(fig_files, "(\\d{2})", "0\\1"))
 
 ## remove prematurely-made "smaller" files
-smaller_files <- list.files(pattern = "lego2.*smaller")
-file.remove(smaller_files)
+# smaller_files <- list.files(pattern = "lego2.*smaller")
+# file.remove(smaller_files)
 
-## main rename for second wave
+## try this thing hadley suggests
+#str_sort(list.files(pattern = "lego3"), numeric = TRUE)
+## neat!
+
+## main rename for third wave
 df <- tibble(
-  new_files = list.files(pattern = "lego2"),
+  new_files = list.files(pattern = "lego3"),
   number = as.integer(str_replace(new_files, "lego2 - (\\d{1,2}).jpg", "\\1")),
   padded =  sprintf("%03d", number + 100),
   new_name = str_c("lego-rstats_", padded, ".jpg")
